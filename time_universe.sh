a8fbe92aaa11eabb8ba449b11070133f8ffd2167
Godfrey Chan <godfreykfc@gmail.com>
2009-09-25 11:06:34 -0700

    Updated timing script to work for all branches

--- time_universe.sh
+++ time_universe.sh
@@ -2,7 +2,8 @@
 
 for population in 10 100 500 $((10**3)) $((10**3 *5))
 do
+    branch = `ruby -e "b = (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '\1').chomp;print (b=='master')?'original':b"`
     echo "Population $population:"
-	time universe -z 0 -f 10 -u 10000 -p $population 1> results/two-sectors/$population.out 2> /dev/null
-	diff results/original/$population.out results/two-sectors/$population.out
+	time universe -z 0 -f 10 -u 10000 -p $population 1> results/$branch/$population.out 2> /dev/null
+	diff results/original/$population.out results/$branch/$population.out
 done
