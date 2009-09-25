27ea82c49ddd8502bfd443c3daa2de9ce30ed7e5
Godfrey Chan <godfreykfc@gmail.com>
2009-09-25 11:14:38 -0700

    Fixed timing script, now creates directory if doesn't already exists

--- time_universe.sh
+++ time_universe.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+BRANCH=`ruby -e "b = (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '\1').chomp;print (b=='master')?'original':b"`
+mkdir results/$BRANCH
+
 for population in 10 100 500 $((10**3)) $((10**3 *5))
 do
-    BRANCH=`ruby -e "b = (%x{git branch 2> /dev/null}.grep(/^\*/).first || '').gsub(/^\* (.+)$/, '\1').chomp;print (b=='master')?'original':b"`
     echo "Population $population:"
 	time universe -z 0 -f 10 -u 10000 -p $population 1> results/$BRANCH/$population.out 2> /dev/null
 	diff results/original/$population.out results/$BRANCH/$population.out
