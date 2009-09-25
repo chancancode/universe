7d55f87ad4311e0a383a572b83639d7b8aedfb12
Godfrey Chan <godfreykfc@gmail.com>
2009-09-25 10:44:36 -0700

    Added diff to timing script

--- time_universe.sh
+++ time_universe.sh
@@ -3,5 +3,6 @@
 for population in 10 100 500 $((10**3)) $((10**3 *5))
 do
     echo "Population $population:"
-	`time universe -z 0 -f 10 -u 10000 -p $population 1> $population.out 2> /dev/null`
+	time universe -z 0 -f 10 -u 10000 -p $population 1> results/two-sectors/$population.out 2> /dev/null
+	diff results/original/$population.out results/two-sectors/$population.out
 done
